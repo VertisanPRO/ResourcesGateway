76c63e0466702544c8d40582ed56c199e25f0fc8
GIGABAIT <59637348+GIGABAIT93@users.noreply.github.com>
2022-01-11 20:12:21 +0200

    Update Lithium.js

--- modules/StripeResources/js/Lithium.js
+++ modules/StripeResources/js/Lithium.js
@@ -1,4 +1,6 @@
-document.querySelector("div .pairs dl").insertAdjacentHTML('afterend', '<dl><dt>Purchases</dt><dd>'+payments_count+'</dd></dl>');
+if (payments_count > 0) {
+  document.querySelector("div .pairs dl").insertAdjacentHTML('afterend', '<dl><dt>Purchases</dt><dd>'+payments_count+'</dd></dl>');
+}
 if (res_id > 0) {
   document.querySelector("main div .ms-auto").innerHTML += '<a href="'+stripe_form_url+'" class="btn btn-primary">'+res_purchase_for_price+' Card</a>';
 }
